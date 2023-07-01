44893443e828e738f2d4a112675c1531e2d4d78c
Hoxton <hoxton019030@gmail.com>
2023-07-01 15:38:46 +0800

    test

--- frontend/src/page/CompanyList.js
+++ frontend/src/page/CompanyList.js
@@ -15,6 +15,7 @@ function CompanyList() {
     }
 
 
+
     return (
 
 
@@ -24,16 +25,17 @@ function CompanyList() {
                 {firms.map((value, index, array) => (
                     <div className="card">
                         <div className="card-header" id="headingOne">
-                            <h2 className="mb-0">
+                            <h2 className="mb-1">
                                 <button className="btn btn-link btn-block text-left" type="button"
                                         data-toggle="collapse"
-                                        data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        data-target="#collapseOne1" aria-expanded="true" aria-controls="collapseOne"
+                                >
                                     {value.auditingAccountingFirm}
                                 </button>
                             </h2>
                         </div>
 
-                        <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
+                        <div id="collapseOne1" className="collapse" aria-labelledby="headingOne"
                              data-parent="#accordionExample">
                             <div className="card-body">
                                 {value.companyList.map((value) => (
@@ -45,41 +47,6 @@ function CompanyList() {
                         </div>
                     </div>
                 ))}
-                <div className="card">
-                    <div className="card-header" id="headingTwo">
-                        <h2 className="mb-0">
-                            <button className="btn btn-link btn-block text-left collapsed" type="button"
-                                    data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
-                                    aria-controls="collapseTwo">
-                                Collapsible Group Item #2
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo"
-                         data-parent="#accordionExample">
-                        <div className="card-body">
-                            Some placeholder content for the second accordion panel. This panel is hidden by default.
-                        </div>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-header" id="headingThree">
-                        <h2 className="mb-0">
-                            <button className="btn btn-link btn-block text-left collapsed" type="button"
-                                    data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
-                                    aria-controls="collapseThree">
-                                Collapsible Group Item #3
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapseThree" className="collapse" aria-labelledby="headingThree"
-                         data-parent="#accordionExample">
-                        <div className="card-body">
-                            And lastly, the placeholder content for the third and final accordion panel. This panel is
-                            hidden by default.
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     )
